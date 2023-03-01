cf8807c8e5bd353c1089962c2d4b02608ea52b39
Allan Valle <allanvalle@outlook.com>
2023-03-01 17:32:51 -0300

    Update-board-and-user-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :boards
-  has_many :rents
+  has_many :boards, dependent: :destroy
+  has_many :rents, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
