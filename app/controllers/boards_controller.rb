015aa4f0122cc1409f00e70baf6350593afc05d6
hurik zamith <hurikzamith@gmail.com>
2023-03-02 12:53:43 -0300

    boards routes e controller, view (new, index, create)

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,33 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(params[:id])
+    @board.save
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+end
