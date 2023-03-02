d4c92d910baf2cb5123d0d08a96dbab44090a43e
hurik zamith <hurikzamith@gmail.com>
2023-03-02 11:29:46 -0300

    seeded users 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating 10 fake users...'
+10.times do
+  User.create!(
+    first_name: Faker::Name.name,
+    last_name: Faker::Name.first_name,
+    email: Faker::Internet.email,
+    address: Faker::Address.street_address,
+    password: Faker::Internet.password
+  )
+end
+puts "#{User.count}Finished!"
