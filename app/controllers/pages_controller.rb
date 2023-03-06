176ef77fb7f2777f6b4037568a3750372d66e045
Mauricio Pacheco <ma-uh@hotmail.com>
2023-03-06 14:46:03 -0300

    Adicionando My Board Games - Collection, My Board Games - Rented Out e Board Games I've Rented ao profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
   def profile
     @user = current_user
-    @board = Board.where(user_id: @user.id)
-    # @board = Board.find(params[:board_id])
-    # @rent = Rent.find(params[:rent_id])
-    # @rents = @board.user.id
-    # @rents = Rent.where(user_id: @user.id, board_id: @board.id)
+    @boards = Board.where(user_id: @user.id)
   end
-
-
 end
